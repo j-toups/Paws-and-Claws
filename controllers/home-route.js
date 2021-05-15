24901779ddeaa6a1aade0b47eaa1c01f4c39ed50
Kirsten Nelson <kirmarnel@gmail.com>
2021-05-14 20:11:06 -0700

    add post and delete requests

--- controllers/home-route.js
+++ controllers/home-route.js
@@ -11,23 +11,10 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
-// router.get('/cats', (req, res) => {
-//     res.render('cats', {
-
-//     });
-// });
-
 router.get('/cats', async (req, res) => {
     try {
-      const catData = await Cat.findAll({
-        // include: [
-        //   {
-        //     model: Cat,
-        //     attributes: ['cat_name', 'age', 'gender', 'description', 'filename'],
-        //   },
-        // ],
-      });
-      console.log(catData)
+      const catData = await Cat.findAll();
+      
   
       const cats = catData.map((cat) =>
         cat.get({ plain: true })
@@ -42,12 +29,32 @@ router.get('/cats', async (req, res) => {
     }
   });
 
+  router.post ('/cats', (req, res) => {
+        Cat.create({
+            cat_name: req.body.cat_name,
+            age: req.body.age,
+            gender: req.body.gender,
+            description: req.body.description,
+            filename: req.body.filename
+        }).then((newCat) => {
+            res.json(newCat);
+          }).catch((err) => res.json(err));
+  });
+
+  router.delete('/cats/:id', (req, res) => {
+    Cat.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then((deletedCat) => {
+      res.json(deletedCat);
+    }).catch((err) => res.json(err));
+  });
+
 
   router.get('/exotics', async (req, res) => {
     try {
-      const fancyData = await Fancy.findAll({
-      
-      });
+      const fancyData = await Fancy.findAll();
   
       const exotics = fancyData.map((exotic) =>
          exotic.get({ plain: true })
@@ -65,37 +72,32 @@ router.get('/cats', async (req, res) => {
 
 module.exports = router;
 
+router.post ('/exotics', (req, res) => {
+    Fancy.create({
+        fancy_name: req.body.fancy_name,
+        age: req.body.age,
+        gender: req.body.gender,
+        description: req.body.description,
+        filename: req.body.filename
+    }).then((newFan) => {
+        res.json(newFan);
+      }).catch((err) => res.json(err));
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+router.delete('/exotics/:id', (req, res) => {
+Fancy.destroy({
+  where: {
+    id: req.params.id
+  }
+}).then((deletedFan) => {
+  res.json(deletedFan);
+}).catch((err) => res.json(err));
+});
 
 
 router.get('/dogs', async (req, res) => {
   try {
-    const dogData = await Dog.findAll({
-      // include: [
-      //   {
-      //     model: Dog,
-      //     attributes: ['dog_name', 'age', 'gender', 'description', 'filename'],
-      //   },
-      // ],
-    });
+    const dogData = await Dog.findAll();
     console.log(dogData)
 
     const dogs = dogData.map((dog) =>
@@ -109,6 +111,28 @@ console.log(dogs)
     console.log(err);
     res.status(500).json(err);
   }
+});
+
+router.post ('/dogs', (req, res) => {
+    Dog.create({
+        dog_name: req.body.dog_name,
+        age: req.body.age,
+        gender: req.body.gender,
+        description: req.body.description,
+        filename: req.body.filename
+    }).then((newDog) => {
+        res.json(newDog);
+      }).catch((err) => res.json(err));
+});
+
+router.delete('/dogs/:id', (req, res) => {
+Dog.destroy({
+  where: {
+    id: req.params.id
+  }
+}).then((deletedDog) => {
+  res.json(deletedDog);
+}).catch((err) => res.json(err));
 });
 
 
